7acb3363718e503575478ad13dfb976f0347fe49
Alexander <okrasnodubets@gmail.com>
2025-04-05 13:55:02 +0200

    Created Admin for Posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ModelPost
 
 # Register your models here.
+
+@admin.register(ModelPost)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'published_date', 'created_at') # What to show in list
+    list_filter = ('published', 'author', 'created_at') # Filters for display list
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('-published_date',)
