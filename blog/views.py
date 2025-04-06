241d9b71fa76c31e521256dd1c5b03c5153baf13
Alexander <okrasnodubets@gmail.com>
2025-04-06 10:14:24 +0200

    PostListView implemented

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ModelPost
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = ModelPost
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = ModelPost.objects.filter(
+        published=True).order_by('-published_date')
+    paginate_by = 5
